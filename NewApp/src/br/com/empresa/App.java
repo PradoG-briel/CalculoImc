1e53018dd06dc5e6a4e8206bd6511a9575226e85
Prado Gabriel <gabriel.prado429@gmail.com>
2021-02-17 11:18:16 -0300

    Implementado calculo de NCD

--- NewApp/src/br/com/empresa/App.java
+++ NewApp/src/br/com/empresa/App.java
@@ -7,11 +7,13 @@ public class App{
 		Aluno alunoPedro = new Aluno();
 		
 		alunoPedro.setNome("Pedro");
-		alunoPedro.setIdade(23);
+		alunoPedro.setIdade(28);
 		alunoPedro.setAltura(1.84);
-		alunoPedro.setPeso(80.4);
-		alunoPedro.setSexo('m');
+		alunoPedro.setPeso(80);
+		alunoPedro.setSexo("M");
+		alunoPedro.setNivelAtividade("Intensa");
 		
 		System.out.println("O seu IMC é = " + alunoPedro.calcularImc());
+		System.out.println("seu ncd é = " + alunoPedro.calcularNcd());
 	}
 }
--- NewApp/src/br/com/empresa/model/Aluno.java
+++ NewApp/src/br/com/empresa/model/Aluno.java
@@ -1,12 +1,12 @@
 package br.com.empresa.model;
-import br.com.empresa.App;
 
 public class Aluno {
 	private String nome;
 	private double altura;
 	private int idade;
-	private char sexo;
+	private String sexo;
 	private double peso;
+	private String nivelAtividade;
 	
 	public void setNome(String nome) {
 		this.nome = nome;
@@ -26,8 +26,8 @@ public class Aluno {
 	public int getIdade() {
 		return this.idade;
 	}
-	public void setSexo(char sexo){
-		if(sexo == 'm' || sexo == 'f') {
+	public void setSexo(String sexo){
+		if(sexo == "M" || sexo == "F") {
 			this.sexo = sexo;
 		}
 		else {
@@ -35,7 +35,7 @@ public class Aluno {
 		}
 		
 	}
-	public char getSexo() {
+	public String getSexo() {
 		return this.sexo;
 	}
 	public void setPeso(double peso) {
@@ -44,12 +44,80 @@ public class Aluno {
 	public double getPeso() {
 		return this.peso;
 	}
+	public void setNivelAtividade(String nivelAtividade) {
+		if(nivelAtividade == "Leve" || nivelAtividade == "Moderado" || nivelAtividade == "Intensa") {
+			this.nivelAtividade = nivelAtividade;
+		}
+		else {
+			System.out.println("Selecione Leve, Moderado ou Intensa ");
+		}
+	}
+	public String getNivelAtividade() {
+		return this.nivelAtividade;
+	}
+
+	
+	public int calcularNcd() {
+		int result = 0;
+		if(nivelAtividade == "Leve" && sexo == "M") {
+			result = (int) (resultadoNcd() * 1.5);
+			return result;
+		}
+		else if(nivelAtividade == "Moderado" && sexo == "M") {
+			result = (int) (resultadoNcd() * 1.8);
+			return result;
+		}
+		else if(nivelAtividade == "Intensa" && sexo == "M") {
+			result = (int) (resultadoNcd() * 2.1);
+			return result;
+		}
+		else if(nivelAtividade == "Leve" && sexo == "F") {
+			result = (int) (resultadoNcd() * 1.6);
+			return result;
+		}
+		else if(nivelAtividade == "Moderado" && sexo == "F") {
+			result = (int) (resultadoNcd() * 1.9);
+			return result;
+		}
+		else if(nivelAtividade == "Intensa" && sexo == "F") {
+			result = (int) (resultadoNcd() * 1.8);
+			return result;
+		}
+		return 0;
+		
+	}
+	
 	public double calcularImc() {
 		double result = (getPeso() + getAltura() * getAltura());
 		return result;
 	}
-//	public double calcularNcd() {
-//		double resul
-//	}
 	
+	public int resultadoNcd() {
+		if(sexo == "M" && idade > 17 && idade < 31) {
+			int result = (int) ((getPeso()*15.3)+679);
+			return result;
+		}
+		else if(idade < 32 && idade < 61) {
+			int result = (int) ((getPeso()*11.6)+879);
+			return result;
+		}
+		else if(idade > 61) {
+			int result = (int) ((getPeso()*13.5)+487);
+			return result;
+		}
+
+		else if(sexo == "F" && idade > 17 && idade < 31) {
+			int result = (int) ((getPeso()*14.7)+496);
+			return result;
+		}
+		else if(idade > 31 && idade < 61) {
+			int result = (int) ((getPeso()*8.7)+829);
+			return result;
+		}
+		else if(idade > 60) {
+			int result = (int) ((getPeso()*10.5)+596);
+			return result;
+		}
+		return 0;
+	}
 }
